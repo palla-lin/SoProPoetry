c9772c9122e29430da44292a4bec73186173e659
Pavle <markovic.pavle@outlook.com>
2021-11-22 11:33:57 +0100

    Initial project structure. Ignore dummy files, will be removed later.

---
+++ utils/strings.py
@@ -0,0 +1,7 @@
+"""
+    This file should contain relevant strings (e.g. paths) frequently used in the project.
+"""
+
+
+ENG_CORPUS_PATH = "data/PoetryFoundationData.csv"
+CHI_CORPUS_PATH = "data/[corpus_name]"
