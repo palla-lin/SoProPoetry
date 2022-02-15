2672b33521968aa93968f9e652e09b2e0e4be68a
Pavle <markovic.pavle@outlook.com>
2022-02-15 17:19:46 +0100

    Changed the script for dataset creation and split. Added notebook for dataset analysis.

--- utils/create_eng_dataset.py
+++ utils/create_eng_dataset.py
@@ -15,33 +15,14 @@ import strings
 from util import read_file
 
 
-ALPHABET = list(string.ascii_letters)
-LINES_LIMIT = 4
-
-
-def process_word(token):
-    if not re.search('[a-zA-Z]', token):
-        return None
-
-    while token[0] not in ALPHABET:
-        token = token[1:]
-    while token[-1] not in ALPHABET:
-        token = token[:-1]
-    
-    return token.lower().strip()
-
-
 def process_lines(lines):
     example = ["[BOP]"]
 
     for line in lines:
-        for word in line.split(' '):
-            word = process_word(word)
-            if word:
-                example += [word]
+        example += line.split(' ')
         example += ["[EOL]"]
 
-    example[-1] = "[EOP]"
+    example += ["[EOP]"]
 
     return example
 
@@ -55,30 +36,36 @@ def process_poems():
     stop_dir = f"{strings.STOPWORDS_LIST_PATH}"
     rake_object = Rake(stop_dir)
 
-    # Open raw data.
-    with open(f"{strings.ENG_POEMS_PATH}", 'rb') as pickle_file:
-        data = pickle.load(pickle_file)
+    # Get cleaned poems
+    with open("clean_poems.json", "r+") as json_file:
+        data = json.load(json_file)
 
     # Make multiple examples from every poem, where a single example is
     # a complete stanza consisting of 4 to 8 lines.
     # Additionally, extract up to 5 keywords for every example.
     # A full example consists of:
-    #     1. poem's lines
-    #     2. topic of the poem
-    #     3. keywords from the selected lines
-    for topic, poems in tqdm(data.items()):
-        for poem in poems:
-            lines = poem.split('\n')[:LINES_LIMIT]
+    #     1. topic of the poem
+    #     2. keywords from the selected lines
+    #     3. poem's lines
+    for _, items in tqdm(data.items()):
+        poem = items["poem"]
+        lines = poem.split('\n')
+        examples = []
 
-            keywords = rake_object.run("\n".join(lines))
-            keywords = list({process_word(pair[0].split(' ')[0]) for pair in keywords[:min(5, len(keywords))]})
-            
-            # Exclude bad examples
+        split_line = random.choice([3, 4, 5])
+        examples.append(lines[:split_line])
+        if len(lines) > 8:
+            examples.append(lines[split_line:split_line+4])
+
+        for example in examples:
+            keywords = rake_object.run("\n".join(example))
+            keywords = list({pair[0].split(' ')[0] for pair in keywords[:min(5, len(keywords))]})
+            keywords = [keyword.split('\n')[0] for keyword in keywords]
+
             if len(keywords) < 2:
                 continue
 
-            example = process_lines(lines)
-            instance = {"topic": topic, "keywords": keywords, "example": example}
+            instance = {"topic": items['topic'], "keywords": keywords, "example": process_lines(example)}
             dataset[f"example_{example_id}"] = instance
             example_id += 1
     
@@ -98,10 +85,10 @@ def topics_generator(topics_list):
 
 
 def generate_set(dataset, topics_dist, is_train=True):
-
     result_set = {}
     num_examples = 6
     id_counter = 0
+
     for ind, topic in enumerate(topics_generator(list(topics_dist.keys())), 1):
         if is_train:
             selected_examples = topics_dist[topic]
@@ -125,28 +112,22 @@ def split_dataset(dataset):
     for key, data in dataset.items():
         topics_dist[data["topic"]].append(key)
     
-    validation_set = generate_set(dataset, topics_dist, False)
+    valid_set = generate_set(dataset, topics_dist, False)
     test_set = generate_set(dataset, topics_dist, False)
     train_set = generate_set(dataset, topics_dist)
 
-    store_dataset(train_set, strings.ENG_TRAIN_PATH)
-    store_dataset(validation_set, strings.ENG_VALID_PATH)
-    store_dataset(test_set, strings.ENG_TEST_PATH)
+    store_dataset(train_set, "train_set.json")
+    store_dataset(valid_set, "valid_set.json")
+    store_dataset(test_set, "test_set.json")
+
+    print(f"Dataset size:   {len(dataset)}")
+    print(f"Train set size: {len(train_set)}")
+    print(f"Valid set size: {len(valid_set)}")
+    print(f"Test  set size: {len(test_set)}")
 
 
 if __name__ == "__main__":
     dataset = process_poems()
-
-    store_dataset(dataset, strings.ENG_CORPUS_PATH)
+    store_dataset(dataset, "eng_dataset.json")
 
     split_dataset(dataset)
-
-    train_set = read_file(strings.ENG_TRAIN_PATH)
-    valid_set = read_file(strings.ENG_VALID_PATH)
-    test_set = read_file(strings.ENG_TEST_PATH)
-    corpus = read_file(strings.ENG_CORPUS_PATH)
-
-    print(f"Train set size: {len(train_set)}")
-    print(f"Valid set size: {len(valid_set)}")
-    print(f"Test  set size: {len(test_set)}")
-    print(f"Corpuss size: {len(corpus)}")
